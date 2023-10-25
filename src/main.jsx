3f572f348fb4f575ca23f2624bc26df855b89d82
CortezWalter <cortezwaltera@gmail.com>
2023-10-24 22:24:39 -0300

    Push
    para arreglo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Landing from './components/Landing.jsx'
+import Landing from './pages/Landing.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
