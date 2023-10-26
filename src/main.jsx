c88f59b9ca14897d84586a5a9aff017aab67d7ae
René Valderrey González <renevalderrey@outlook.com>
2023-10-26 11:35:25 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Landing from "./pages/Landing.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createHashRouter({
+  path: "/",
+  element: <Landing />,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}>
+    <App />
+  </RouterProvider>
+);
