1d7b1a80917eda26b473ab00c759210ee8c6662d
René Valderrey González <renevalderrey@outlook.com>
2023-10-26 11:40:52 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rmwebs2023.github.io/planning-poker/",
+  base: "https://RMWebs2023.github.io/planning-poker/",
 });
